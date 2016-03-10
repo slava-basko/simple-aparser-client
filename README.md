2a2e7f2be9639de8396713659d4b4672ede62c27
Slava Basko <vyacheslav.b@mserverone.com>
2016-03-10 11:36:06 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Simple A-Parser Client
+======================
---
+++ src/Aparser/Exceptions/CurlResponseException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by Slava Basko <basko.slava@gmail.com>
+ * Date: 3/10/16
+ */
+
+namespace Aparser\Exceptions;
+
+
+class CurlResponseException extends \Exception
+{
+
+}
